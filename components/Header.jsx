f7e620b3a898d3be9ad42b2af7421ba0e91ea7b3
aoihaugen <51489207+aoihaugen@users.noreply.github.com>
2023-02-05 21:05:48 +0100

    Added home link in Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import styles from "./Header.module.css";
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>⚡ My electricity usage history ⚡</h1>
+      <Link href="/">Hjem</Link>
+      <h1>⚡ Hvilken leverandør burde jeg hatt forrige måned?⚡</h1>
     </header>
   );
 }
