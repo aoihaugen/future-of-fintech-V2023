92c65c11a5def36cd6d782d5c9f64a7a8e14904b
Andreas Øihaugen <aoihaugen@gmail.com>
2023-02-06 17:37:01 +0100

    Cleanup
    Deleted not needed legacy files from original project.

--- pages/api/providers.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import providers from "../../data/providers.json";
-
-export default function handler(req, res) {
-  res.status(200).json(providers);
-}
